e9656f5afa7fe66510598b4b5d956cd970b36034
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-10 03:39:13 +0000

    first commit

--- MallFinder-cli/lib/MallFinder/cli.rb
+++
@@ -1,8 +0,0 @@
-require "MallFinder/cli/version"
-
-module MallFinder
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- MallFinder-cli/lib/MallFinder/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module MallFinder
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
